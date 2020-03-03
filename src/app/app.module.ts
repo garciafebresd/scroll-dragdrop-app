92737d4dd98e4a708def3d9ed8ba4c23a10643b3
daniel garcia <dgarcia@position.cl>
2020-03-03 14:50:09 -0300

    adding some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+//Components
+// import { DragComponent } from './components/drag/drag.component';
+// import { PaisesComponent } from './components/paises/paises.component';
+import { VirtualComponent } from './components/virtual/virtual.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // DragComponent,
+    // PaisesComponent,
+    VirtualComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ScrollingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
